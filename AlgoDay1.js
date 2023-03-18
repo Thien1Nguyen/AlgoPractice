0583dc93eb8e47541ddfd7ffad96d3ff58362316
Thien1Nguyen <tnguyen5219@gmail.com>
2023-03-18 06:19:11 -0700

    Day 1

---
+++ AlgoDay1.js
@@ -0,0 +1,16 @@
+// CHAPTER 1 – FUNDAMENTALS
+// Sigma
+function sigma(num){
+    var given = 0;
+    for(var i = 1; i<=num; i++){
+        given += i;
+    }
+    return given
+}
+
+console.log(sigma(3));
+
+// Factorial
+function factorial(num){
+    
+}
